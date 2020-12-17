85fd41dff011195eac554c4cdb7ab50951abae16
Mofasser Hossain <72354462+MofasserHossain@users.noreply.github.com>
2020-12-17 18:46:03 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -79,6 +79,17 @@ window.addEventListener("scroll", function () {
     });
     
   });
+  
+    function countdown() {
+      var i = document.getElementById('count-number');
+      if (parseInt(i.innerHTML)<=0) {
+          location.href = '/';
+      }
+  if (parseInt(i.innerHTML)!=0) {
+      i.innerHTML = parseInt(i.innerHTML)-1;
+  }
+  }
+  setInterval(function(){ countdown(); },1000);
 })(jQuery);
 
 
